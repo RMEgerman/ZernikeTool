f563954a21b25b36fe9cbb9f89416bc5b1d7b8fe
RMEgerman <123949753+RMEgerman@users.noreply.github.com>
2023-02-23 12:33:56 +0100

    Update ZernikeFunctions2.py

--- ZernikeFunctions2.py
+++ ZernikeFunctions2.py
@@ -193,7 +193,7 @@ def ZernikeTerms():
 def ZernikeDecomposition(rho,phi,m_max,dz,UnitFactor):
     A = [[0,0]]
 
-    for i in range(1,55):
+    for i in range(1,m_max):
 #        for j in range(-i,i+1,2):
 #            A.append([j,i])
         nnn=int(np.ceil((-3+np.sqrt(9+8*i))/2.))
