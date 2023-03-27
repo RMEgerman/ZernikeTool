d53c1785461a4ce1570b274a0c8df87166ff78d1
RMEgerman <123949753+RMEgerman@users.noreply.github.com>
2023-03-27 12:26:52 +0200

    Update ZernikeFunctions2.py

--- ZernikeFunctions2.py
+++ ZernikeFunctions2.py
@@ -448,9 +448,11 @@ def main():
                 col1, col2,col3,col4,col5,col6 = st.columns(6)
                 H = [col1,col2,col3,col4,col5,col6]
                 
-                for j in range(len(ZernikeModeNames)-1,-1,-1):
-                    # i = np.argsort(SFEs)[-1-j]
-                    i=j
+                # for j in range(len(ZernikeModeNames)-1,-1,-1):
+                #     # i = np.argsort(SFEs)[-1-j]
+                #     i=j
+                for j in range(len(ZernikeModeNames)):
+                    i = np.argsort(SFEs)[-1-j]
                     plt.figure(i+1)
                     Zjan = griddata((x,y),ZernikeInfluenceFunctions[:,i],(xi,yi),method='cubic')
                     fig,ax = plt.subplots(figsize=(6,3))
