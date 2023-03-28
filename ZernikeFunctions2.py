8b8175529b55c199950a6dec5608a6f6f987b9a1
RMEgerman <123949753+RMEgerman@users.noreply.github.com>
2023-03-28 13:59:20 +0200

    Update ZernikeFunctions2.py

--- ZernikeFunctions2.py
+++ ZernikeFunctions2.py
@@ -200,8 +200,6 @@ def ZernikeDecomposition(rho,phi,m_max,dz,UnitFactor):
     else:
         m_max2=m_max
     for i in range(1,m_max2):
-#        for j in range(-i,i+1,2):
-#            A.append([j,i])
         nnn=int(np.ceil((-3+np.sqrt(9+8*i))/2.))
         mmm=int(2*i-(nnn*(nnn+2)))
         A.append([mmm,nnn])
@@ -219,7 +217,6 @@ def ZernikeDecomposition(rho,phi,m_max,dz,UnitFactor):
         Zs = np.zeros([len(rho),k_inf+1])
         
         if (abs(m)-n == 0) and (m == 0):
-            #print('boe')
             k = 0
             F1 = np.math.factorial(n-k)
             F2 = np.math.factorial(k)
@@ -244,7 +241,6 @@ def ZernikeDecomposition(rho,phi,m_max,dz,UnitFactor):
 
         ZernikeInfluenceFunctions[:,i] = Zs
         
-    #Xlinear = np.linalg.lstsq(ZernikeInfluenceFunctions,dz,rcond=None)[0] 
     Xlinear = np.dot(np.linalg.pinv(ZernikeInfluenceFunctions),dz)
     Zernikes = Xlinear*ZernikeInfluenceFunctions
     if (m_max == 4):
@@ -266,6 +262,113 @@ def ZernikeDecomposition(rho,phi,m_max,dz,UnitFactor):
     PVs = np.round((np.max(Zernikes,axis=0) - np.min(Zernikes,axis=0)) * UnitFactor,3)
     return Zernikes, ZernikeInfluenceFunctions, Xlinear,m,A,SFEs,PVs,mnlist
 
+def CalcZernikeResiduals(rho,phi,dz,UnitFactor,ZernikeNames2):
+    A = [[0,0]]
+
+    for i in range(1,85):
+        nnn=int(np.ceil((-3+np.sqrt(9+8*i))/2.))
+        mmm=int(2*i-(nnn*(nnn+2)))
+        A.append([mmm,nnn])
+#    mnlist = ['Z[' + str(A[0][0]) + ']' +'[' + str(A[0][1]) + ']']        
+#    for i in range(1,len(A)):
+#        mnlist.append('Z[' + str(A[i][0]) + ']' +'[' + str(A[i][1]) + ']')
+    
+    ZernikeInfluenceFunctions = np.zeros([len(rho),len(A)])
+    for i in range(len(A)):
+        
+        m = A[i][0]
+        n = A[i][1]
+        k_inf = int(((n-abs(m))/2))
+    
+        Zs = np.zeros([len(rho),k_inf+1])
+        
+        if (abs(m)-n == 0) and (m == 0):
+            k = 0
+            F1 = np.math.factorial(n-k)
+            F2 = np.math.factorial(k)
+            F3 = np.math.factorial(int((n+abs(m))/2) - k )
+            F4 = np.math.factorial(int((n-abs(m))/2) - k )
+            Zs = (-1)**k*F1/(F2*F3*F4)*rho**(n-2*k)
+        else:
+            
+            for k in range(int((n-abs(m))/2)+1):
+                F1 = np.math.factorial(n-k)
+                F2 = np.math.factorial(k)
+                F3 = np.math.factorial(int((n+abs(m))/2) - k )
+                F4 = np.math.factorial(int((n-abs(m))/2) - k )
+                Ri = (-1)**k*F1/(F2*F3*F4)*rho**(n-2*k)
+                Zs[:,k] = Ri  
+            Zs = np.sum(Zs,axis=1)
+        
+        if m <= 0:    
+            Zs = Zs.reshape(len(Zs))*np.cos(abs(m)*phi)
+        else:
+            Zs = Zs.reshape(len(Zs))*np.sin(abs(m)*phi)
+
+        ZernikeInfluenceFunctions[:,i] = Zs
+        
+    Xlinear = np.dot(np.linalg.pinv(ZernikeInfluenceFunctions),dz)
+    Zernikes = Xlinear*ZernikeInfluenceFunctions
+    # SFEs = np.zeros((m_max))
+    # for i in range(m_max):
+    #     RMS = np.round(rms(Zernikes[:,i]) * UnitFactor,3)
+    #     SFEs[i] = RMS
+    PVs = np.round((np.max(Zernikes,axis=0) - np.min(Zernikes,axis=0)) * UnitFactor,3)
+    DZ=dz
+    j=0
+    standard_seq=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+    print('standard')
+    print(standard_seq)    
+    fringe_seq=[0,1,2,4,3,5,7,8,12,6,9,11,13,17,18,24,10,14,16,19,23,25,31,32,40,15,20,22,26,30,33,39,41,49,50,60,84]
+    print('fringe')
+    print(fringe_seq)        
+    B = [A[i] for i in fringe_seq]
+    PVs[standard_seq] = PVs[fringe_seq]
+    Zernikes[:,standard_seq] = Zernikes[:,fringe_seq]
+    Residuals=np.empty(shape=[23,5],dtype="<U10")
+    Residuals[0,:]=['Input Surface','','',str(rms(DZ) * UnitFactor),str(np.round((np.max(DZ) - np.min(DZ)) * UnitFactor,3))]
+    j=1
+    for i in range(len(fringe_seq)-1):
+        m = B[i][0]
+        n = B[i][1]
+        if (m ==0 ) and (n == 0):
+            DZ=DZ-Zernikes[:,0]
+            mag=str(np.format_float_positional( Zernikes[0,0] * UnitFactor ,precision=4))
+            phase='0.0'
+            resPV=str(np.round((np.max(DZ,axis=0) - np.min(DZ,axis=0)) * UnitFactor,3))
+            resRMS=str(np.round(rms(DZ) * UnitFactor,3))
+            Residuals[j,:]=[ZernikeNames2[j-1],mag,phase,resRMS,resPV]
+
+            j=j+1
+        elif (m == 0) and (n > 0):
+            DZ=DZ-Zernikes[:,i]
+            mag=str(np.format_float_positional((PVs[i]/2.),precision=4))
+            phase='0.0'
+            resPV=str(np.round((np.max(DZ,axis=0) - np.min(DZ,axis=0)) * UnitFactor,3))
+            resRMS=str(np.round(rms(DZ) * UnitFactor,3))
+            Residuals[j,:]=[ZernikeNames2[j-1],mag,phase,resRMS,resPV]
+            j=j+1
+        elif (m > 0) and (n > 0):
+            for k in range(len(fringe_seq)):
+
+                if (-(B[k][0]) == B[i][0]) and (B[k][1] == B[i][1]):
+                    l=k
+                    break
+            
+            DZ=DZ - Zernikes[:,i] - Zernikes[:,l]
+            mag=str( np.format_float_positional(np.sqrt(((PVs[i]/2)**2)+((PVs[l]/2))**2),precision=4  ))
+            phase=str(np.arctan2(PVs[i],PVs[l])*180./np.pi)
+            resPV=str(np.round((np.max(DZ,axis=0) - np.min(DZ,axis=0)) * UnitFactor,3))
+            resRMS=str(np.round(rms(DZ) * UnitFactor,3))
+            Residuals[j,:]=[ZernikeNames2[j-1],mag,phase,resRMS,resPV]
+            j=j+1
+        else:
+            continue
+ 
+    return Residuals
+
+
+
 
 def ZernikeNamesFunc(m_max):
     ZernikeNames = [' Piston',' Tip',' Tilt',' Astigmatism 1', ' Defocus',' Astigmatism 2',' Trefoil 1',
@@ -275,20 +378,32 @@ def ZernikeNamesFunc(m_max):
     if (m_max == 4):
         ro = [0,1,2,4,3,5,6,7,8,9,10,11,12]
         ZernikeNames = [val for (_, val) in sorted(zip(ro, ZernikeNames), key=lambda x: x[0])]
-    return ZernikeNames        
+    return ZernikeNames   
+
+def ZernikeNamesFunc2():
+    ZernikeNames2 = [' Piston',' Tip/Tilt',' Defocus',' Pri Astigmatism',' Pri Coma',
+                    ' Pri Spherical', ' PriTrefoil',' Sec Astigmatism',' Sec Coma', ' Sec Spherical', ' Pri Tetrafoil',
+                    ' Sec Trefoil',' Ter Astigmatism',' Ter Coma',' Ter Spherical',' Pri Pentafoil',' Sec Tetrafoil',
+                    ' Ter Trefoil',' Qua Astigmatism',' Qua Coma',' Qua Spherical',' Quin Spherical']
+    for i in range(1000,4):
+        ZernikeNames2.append(' ')
+    return ZernikeNames2      
 
 def ZernikeTableFunc(mnlist, ZernikeNames, m_max):
     ZernikeTable = []
     ZernikeNames = ZernikeNamesFunc(m_max)
 
     for i in range(len(mnlist)):
-        ZernikeTable.append(str(mnlist[i])+ZernikeNames[i])
-    ZernikeTable.append(' ')
-    ZernikeTable.append('Original data:')
-    ZernikeTable.append('Quadratic Sum Zernike Terms:')
-    ZernikeTable.append('Residual error:')
-        
+        ZernikeTable.append(str(mnlist[i])+ZernikeNames[i])     
     return ZernikeTable
+
+def ZernikeTableFunc2(mnlist, ZernikeNames, m_max):
+    ZernikeTable2 = []
+    ZernikeNames2 = ZernikeNamesFunc2()
+
+    for i in range(22):
+        ZernikeTable2.append(ZernikeNames2[i])       
+    return ZernikeTable2
 
 def PistonTipTiltTableFunc(Xlinear, PTT, PVs, Rmax, UnitFactor, Zernikes):
 #   PistonTable = [str(np.format_float_scientific(PTT[0],precision=4))]
@@ -443,7 +558,10 @@ def main():
             ZernikeNames = ZernikeNamesFunc(m_max)
             ZernikeTable = ZernikeTableFunc(mnlist, ZernikeNames, m_max)
             
-
+            Residuals = CalcZernikeResiduals(rho,phi,dz,UnitFactor)            
+            ZernikeNames2 = ZernikeNamesFunc2()
+            ZernikeTable2 = ZernikeTableFunc2(mnlist, ZernikeNames2, m_max)
+       
             with st.expander('Zernike decompostion plots, sorted'):
                 col1, col2,col3,col4,col5,col6 = st.columns(6)
                 H = [col1,col2,col3,col4,col5,col6]
@@ -500,6 +618,11 @@ def main():
                 #st.write(dfTable) 
 
                 st.table(dfTable.style)
+                
+            with st.expander('Zernike Residual Table - Mag/Phase thru Quin Spherical'):
+                dfTable = pd.DataFrame({'Zernike: Mag [nm] Phase[deg] ResRMS[nm] resPV[nm]' : ZernikeTable2}) 
+                st.table(dfTable.style)
+               
                 
         with st.expander('X and Y locations of all datapoints'):
             plotly_function(x,y,'data coordinates')
